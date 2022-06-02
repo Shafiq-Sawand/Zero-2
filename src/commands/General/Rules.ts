81b6f815e885b6fe3919b4e58f051e021890bae7
Shafiq Sawand <105638767+Shafiq-Sawand@users.noreply.github.com>
2022-06-02 13:31:32 +0500

    Update Rules.ts

--- src/commands/General/Rules.ts
+++ src/commands/General/Rules.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
         let zerotwo = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: zerotwo }, MessageType.video, {
             mimetype: Mimetype.gif,
-            caption: `_*----📍[Rules for Zero Two's usage]📍----*_\n\n\n*>>>* use -support to get the Official group link in your dm.\n\n*--->*  If you want to chat with Zero Two you can use -chat *Don't chat in private bot can't chat in Pm.\n\n*--->* If you want to add Zero Two in your group the contact the owner by *-owner/-mods* \n\n*--->* Dont use wrong command, use the command given in the *-help* list \n\n*--->* Dont spam the bot with commands if Zero Two is not responding, its means the maybe owner is offline or facing internet issue. \n\n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BANNED* 🚫 \n\n\n*©️ Synthesized Infinity Bots* ` }
+            caption: `_*----📍[Rules for Zero 2's usage]📍----*_\n\n\n*>>>* use #support to get the Official group link in your dm.\n\n*--->*  If you want to chat with Zero Two you can use #chat *Don't chat in private bot can't chat in Dm.\n\n*--->* If you want to add Zero Two in your group the contact the owner by *#owner/#mods* \n\n*--->* Dont use wrong command, use the command given in the *#help* list \n\n*--->* Dont spam the bot with commands if Zero Two is not responding, its means the maybe owner is offline or facing internet issue. \n\n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BANNED* 🚫 \n\n\n*©️ SHAFIQ_KHAN_SAWAND* ` }
         )
     }
 }
