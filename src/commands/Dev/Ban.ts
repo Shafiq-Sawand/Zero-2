3990443f7e3e6719cdcaac56f21d31e64115b1de
Shafiq Sawand <105638767+Shafiq-Sawand@users.noreply.github.com>
2022-06-04 11:43:19 +0500

    Update Ban.ts

--- src/commands/Dev/Ban.ts
+++ src/commands/Dev/Ban.ts
@@ -11,7 +11,7 @@ export default class Command extends BaseCommand {
             category: 'dev',
             usage: `${client.config.prefix}ban [@tag]`,
             modsOnly: true,
-            baseXp: 200
+            baseXp: 200000
         })
     }
 
